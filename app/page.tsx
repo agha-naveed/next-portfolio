bd6be9fc72f7ab4abb2ac6d0bbb46bae1374626a
agha-naveed <naveedabs31@gmail.com>
2025-05-01 21:26:19 +0500

    about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import About from "./components/About"
 import Header from "./components/Header"
 
 export default function Page() {
   return (
-    <div className="w-full h-full bg-my-clr p-4">
+    <div className="w-full h-full p-4">
       <Header />
+      <About />
     </div>
   )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  server: {
+    host: "0.0.0.0"
+  }
 };
 
 export default nextConfig;
