4033d0b034de96fa77b054a36b98716e770f31c6
agha-naveed <naveedabs31@gmail.com>
2025-05-01 21:49:32 +0500

    adding framer option

---
+++ app/components/Skills.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { useState } from "react"
+import { AnimatePresence, motion } from "motion/react"
+
+export default function Skills() {
+    const [option, setOption] = useState("")
+    return (
+        <div className='w-full h-full p-sec bg-main-dark-clr'>
+            <div className='container !mx-auto justify-items-center'>
+                <h2 className='text-5xl font-bold text-white text-center'>Skills</h2>
+                <div className="flex gap-3 justify-between w-[80%]">
+                    <div className="text-white text-start flex flex-col gap-1 w-65">
+                        <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start" onClick={(e) => setOption("frontend")}>Frontend</button>
+                        <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start" onClick={(e) => setOption("backend")}>Backend</button>
+                        <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start" onClick={(e) => setOption("database")}>Database</button>
+                        <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start">Programming</button>
+                        <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start">Designing</button>
+                        <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start">AI</button>
+                        <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start">Tools</button>
+                    </div>
+                    <div className="w-1 h-100 bg-main-skin-clr"></div>
+                    <div className="w-full h-full bg-purple-700">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import About from "./components/About"
 import Header from "./components/Header"
+import Skills from "./components/Skills"
 
 export default function Page() {
   return (
     <div className="w-full h-full p-4">
       <Header />
       <About />
+      <Skills />
     </div>
   )
 }
