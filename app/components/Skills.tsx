9c9709e9520cd5ebda073f9526dd9eb5853f82eb
agha-naveed <naveedabs31@gmail.com>
2025-05-02 12:11:50 +0500

    .

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -9,13 +9,14 @@ import react from 'public/img/skills-logo/react.svg'
 import next from 'public/img/skills-logo/nextjs.svg'
 
 export default function Skills() {
-    const [option, setOption] = useState("")
+    
+    const [option, setOption] = useState("frontend")
     return (
         <div className='w-full h-full p-sec bg-main-dark-clr relative -top-1'>
             <div className='container !mx-auto justify-items-center'>
                 <h2 className='text-5xl font-bold text-white text-center'>SKILLS</h2>
 
-                <div className="flex gap-3 justify-between w-[80%] !mt-12">
+                <div className="flex gap-3 justify-center !mt-12">
                     <div className="text-white text-start flex flex-col gap-1 w-65">
                         <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start" onClick={(e) => setOption("frontend")}>Frontend</button>
                         <button className="border-none outline-none !py-[13px] w-full hover:underline cursor-pointer text-xl text-start" onClick={(e) => setOption("backend")}>Backend</button>
@@ -28,8 +29,38 @@ export default function Skills() {
 
                     <div className="w-1 h-100 bg-main-skin-clr"></div>
 
-                    <div className="w-full h-full text-white">
-                        <div className="grid grid-cols-3 gap-5 w-1/2">
+                    <div className="w-180 h-full text-white">
+                        {
+                            // option == "frontend" ?
+                            <div className={`transition-all ${show ? "scale-100 opacity-100" : "scale-90 opacity-0"} grid grid-cols-4 gap-5 w-110 !pt-2 !pl-10`}>
+                                <div className="flex flex-col gap-1 items-center">
+                                    <Image src={html} className="w-10" alt="" />
+                                    <label htmlFor="">HTML</label>
+                                </div>
+                                <div className="flex flex-col gap-1 items-center">
+                                    <Image src={css} className="w-10" alt="" />
+                                    <label htmlFor="">CSS</label>
+                                </div>
+                                <div className="flex flex-col gap-1 items-center">
+                                    <Image src={js} className="w-10" alt="" />
+                                    <label htmlFor="">JavaScript</label>
+                                </div>
+                                <div className="flex flex-col gap-1 items-center">
+                                    <Image src={ts} className="w-10" alt="" />
+                                    <label htmlFor="">TypeScript</label>
+                                </div>
+                                <div className="flex flex-col gap-1 items-center">
+                                    <Image src={react} className="w-10" alt="" />
+                                    <label htmlFor="">React Js</label>
+                                </div>
+                                <div className="flex flex-col gap-1 items-center">
+                                    <Image src={next} className="w-10" alt="" />
+                                    <label htmlFor="">Next Js</label>
+                                </div>
+                            </div>
+                            // : ""
+                        }
+                        {/* <div className={`transition-all ${option == "frontend" ? "scale-100 opacity-100" : "scale-90 opacity-0"} grid grid-cols-4 gap-5 w-110 !pt-2 !pl-10`}>
                             <div className="flex flex-col gap-1 items-center">
                                 <Image src={html} className="w-10" alt="" />
                                 <label htmlFor="">HTML</label>
@@ -54,7 +85,7 @@ export default function Skills() {
                                 <Image src={next} className="w-10" alt="" />
                                 <label htmlFor="">Next Js</label>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
