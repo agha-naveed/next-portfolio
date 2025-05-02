75bdec5ec76a7aca3a07febdf16ca515e6fa679c
agha-naveed <naveedabs31@gmail.com>
2025-05-02 13:03:03 +0500

    .

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import html from 'public/img/skills-logo/html-logo.svg'
 import css from 'public/img/skills-logo/css-logo.svg'
@@ -9,8 +9,8 @@ import react from 'public/img/skills-logo/react.svg'
 import next from 'public/img/skills-logo/nextjs.svg'
 
 export default function Skills() {
-    
     const [option, setOption] = useState("frontend")
+
     return (
         <div className='w-full h-full p-sec bg-main-dark-clr relative -top-1'>
             <div className='container !mx-auto justify-items-center'>
@@ -31,8 +31,8 @@ export default function Skills() {
 
                     <div className="w-180 h-full text-white">
                         {
-                            // option == "frontend" ?
-                            <div className={`transition-all ${show ? "scale-100 opacity-100" : "scale-90 opacity-0"} grid grid-cols-4 gap-5 w-110 !pt-2 !pl-10`}>
+                            option == "frontend" ?
+                            <div className={`transition-all ${option == "frontend" ? "grid" : "hidden"} ${option ? "scale-100 opacity-100" : "scale-90 opacity-0"} grid-cols-4 gap-5 w-110 !pt-2 !pl-10`}>
                                 <div className="flex flex-col gap-1 items-center">
                                     <Image src={html} className="w-10" alt="" />
                                     <label htmlFor="">HTML</label>
@@ -58,7 +58,7 @@ export default function Skills() {
                                     <label htmlFor="">Next Js</label>
                                 </div>
                             </div>
-                            // : ""
+                            : ""
                         }
                         {/* <div className={`transition-all ${option == "frontend" ? "scale-100 opacity-100" : "scale-90 opacity-0"} grid grid-cols-4 gap-5 w-110 !pt-2 !pl-10`}>
                             <div className="flex flex-col gap-1 items-center">
