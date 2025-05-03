75561c5fa08117fca6ba06b0f0b720db0d8dc997
agha-naveed <naveedabs31@gmail.com>
2025-05-03 17:42:13 +0500

    ...

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -12,13 +12,14 @@ export default function Projects() {
                 <h2 className='text-5xl font-semibold text-white text-center'>PROJECTS</h2>
 
                 <div className='w-fit bg-secondary-light-clr !py-3 !px-5 rounded-lg'>
-                    <span className='font-semibold text-xl block !py-3'>Library Management System</span>
-                    <Image src={store} alt='' className='w-80 rounded-sm' placeholder='blur' />
+                    <span className='font-semibold text-2xl block !py-[14px]'>Library Management System</span>
+                    <Image src={store} alt='' className='w-100 rounded-sm' placeholder='blur' />
 
-                    <div className='flex flex-col !py-3'>
-                        <span className='text-xl font-semibold'>Library Management System</span>
-                        <span className='font-semibold'>Desktop Application</span>
+                    <div className='flex flex-col !py-8 gap-1'>
+                        <span className='text-[22px] font-medium'>Library Management System</span>
+                        <span className='text-[18px] font-medium'>Desktop Application</span>
                     </div>
+                    <button className='!px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all hover:bg-main-skin-clr' title='View more about this Project'>View More</button>
                 </div>
 
             </div>
