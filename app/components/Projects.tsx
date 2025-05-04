f9418477d57b343068387698a3149549fa1158f6
agha-naveed <naveedabs31@gmail.com>
2025-05-04 12:09:03 +0500

    ...

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -23,28 +23,21 @@ export default function Projects() {
     }
     
     return (
-        <div className='w-full h-full !py-10 overflow-hidden bg-main-dark-clr relative -top-3'>
+        <div className='w-full h-full !py-10 overflow-hidden bg-main-dark-clr relative -top-3 z-300'>
             <div className="container !mx-auto text-white">
 
                 <h2 className='md:text-5xl text-[40px] font-semibold text-white text-center !mb-15'>PROJECTS</h2>
 
                 <div className='flex flex-wrap justify-center relative gap-14'>
 
+                    
                     <Image src={dottedBg} alt=''
                     className='mix-blend-screen opacity-15
-                    absolute top-20 -left-60 w-[70%]
-                    '
-                    />
+                    absolute -top-60 -left-60 w-[70%] pointer-events-none'/>
 
-                    <Image src={blueFlare}
-                    className='absolute
-                    mix-blend-screen
-                    animate-blink
-                    top-70
-                    left-30
-                    w-[350px]
-                    '
-                    alt='' />
+                    <Image src={blueFlare} alt=''
+                    className='mix-blend-screen absolute -top-30 -left-10 w-70 animate-blink'
+                    />
 
                     <div data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-out">
                         <Tilt options={defaultOptions}
@@ -55,8 +48,8 @@ export default function Projects() {
                             </div>
 
                             <div className='flex flex-col !py-8 gap-1'>
-                                <span className='text-[22px] font-medium'>Lenmi Store</span>
-                                <span className='text-[18px] font-medium'>Online Shopping Platform</span>
+                                <span className='text-[20px] font-semibold'>Lenmi Store</span>
+                                <span>Online Shopping Platform</span>
                             </div>
                             <button className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all hover:bg-main-skin-clr w-fit !mb-5 absolute bottom-[16px]' title='View more about this Project'>
                                 <span className='relative z-[200]'>View More</span>
@@ -73,8 +66,8 @@ export default function Projects() {
                             </div>
 
                             <div className='flex flex-col !py-8 gap-1'>
-                                <span className='text-[22px] font-medium'>Library Management System</span>
-                                <span className='text-[18px] font-medium'>Desktop Application</span>
+                                <span className='text-[20px] font-semibold'>Library Management System</span>
+                                <span>Desktop Application</span>
                             </div>
                             <button className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all hover:bg-main-skin-clr w-fit !mb-5 absolute bottom-[16px]' title='View more about this Project'>
                                 <span className='relative z-[200]'>View More</span>
@@ -84,15 +77,15 @@ export default function Projects() {
                     </div>
                     
                     <div data-aos="fade-up" data-aos-duration="1600" data-aos-easing="ease-out">
-                        <Tilt options={defaultOptions} className='md:w-[350px] w-[80%] relative bg-secondary-light-clr !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[100]'>
+                        <Tilt options={defaultOptions} className='md:w-[350px] w-[80%] relative bg-secondary-light-clr !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[1000]'>
                             
                             <div className='w-full overflow-hidden rounded-sm'>
                                 <Image src={chatbot} alt='' className='w-full rounded-sm hover:scale-105 transition-all' placeholder='blur' />
                             </div>
 
                             <div className='flex flex-col !py-8 gap-1'>
-                                <span className='text-[22px] font-medium'>AI Chatbot</span>
-                                <span className='text-[18px] font-medium'>Agha Chatbot</span>
+                                <span className='text-[20px] font-semibold'>AI Chatbot</span>
+                                <span>Agha Chatbot</span>
                             </div>
                             <button className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all hover:bg-main-skin-clr w-fit !mb-5 absolute bottom-[16px]' title='View more about this Project'>
                                 <span className='relative z-[200]'>View More</span>
@@ -106,10 +99,13 @@ export default function Projects() {
 
                 <div className='!py-30 !px-2 relative'>
                     <Image src={dottedBg} alt=''
-                        className='mix-blend-screen opacity-10
-                        absolute bottom-10 -right-70 rotate-270 w-[70%] !select-none
-                        '
-                    />
+                        className='mix-blend-screen opacity-10 pointer-events-none
+                        absolute bottom-10 -right-70 rotate-270 w-[70%] !select-none'/>
+
+                        <Image src={blueFlare} alt=''
+                        className='mix-blend-screen absolute top-20 right-0 w-70 animate-blink'
+                        />
+
                     <h2 className='md:text-5xl text-[40px] font-semibold text-white text-center !mb-15'>OTHER PROJECTS</h2>
 
                     <div className='flex flex-wrap justify-center relative gap-14'>
