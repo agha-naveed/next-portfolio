8c7f6631f669ed31fbd59a653c38ea32d7b68c98
agha-naveed <naveedabs31@gmail.com>
2025-05-05 09:06:32 +0500

    scroll to top

--- app/page.tsx
+++ app/page.tsx
@@ -10,27 +10,25 @@ import { RiArrowUpDoubleLine } from "react-icons/ri";
 
 export default function Page() {
   const [scroll, setScroll] = useState(false)
+
   useEffect(() => {
     AOS.init()
+
     const handleScroll = () => {
-      if(window.scrollY < 50) {
-        setScroll(false)
-      }
-      else if(window.scrollY > 50 && window.scrollY < 100) {
-        setScroll(true)
-      } 
+      setScroll(window.scrollY > 50)
     }
     window.addEventListener("scroll", handleScroll)
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
-    
+    return () => window.removeEventListener("scroll", handleScroll)
+  
   }, []);
 
   return (
     <div className="w-full h-full p-4">
-      <RiArrowUpDoubleLine className="fixed bottom-0 p-2" />
+      <RiArrowUpDoubleLine
+      className={`fixed right-10 !p-2 h-12 w-12 text-white text-4xl transition-custom
+        rounded-full ${scroll ? "bottom-10" : "-bottom-20"} z-[4000] hover-text hover:bg-black hover:shadow-arrow
+      `} onClick={() => window.scrollTo({top: 0, behavior: "smooth"})} />
       <Header />
       <About />
       <Skills />
