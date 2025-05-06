c07555b904f4c881d3b3a34a0d6b528439fe52da
agha-naveed <naveedabs31@gmail.com>
2025-05-06 09:56:50 +0500

    .

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center text-white">
+            <h1 className="text-7xl flex animate-bounce">404</h1>
+            <p>Page Not Found</p>
+        </div>
+    )
+}
