e7a28f46736bd761045bd9d9cfcf96f8341f8d2d
agha-naveed <naveedabs31@gmail.com>
2025-05-07 10:24:18 +0500

    flex reverse

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,21 +6,43 @@ export default function page() {
     return (
         <div className='w-full h-full bg-main-dark-clr'>
             <div className="container !mx-auto">
-                <div className='w-full h-screen flex justify-center items-center gap-10 !pt-50 !px-4'>
-                    <div className='w-1/2 text-white text-[18px]'>
+                <div className='w-full h-screen flex flex-row-reverse justify-center items-center gap-10 !pt-40 !px-4'>
+                    <div className='w-1/2 h-[100%] max-h-fit text-white text-[18px] flex flex-col place-content-start gap-20 overflow-y-auto !pr-4 text-justify'>
                         
+                        <span className='flex flex-col gap-3'>
+                            <h3 className='text-4xl font-semibold'>About</h3>
+                            <p>👋Hi, My name is Syed Naveed Abbas, and I’m a passionate Full Stack Engineer from Skardu, Pakistan.
+                            <br />
+                            I enjoy building modern, responsive, and dynamic web applications that solve real-world problems. With strong knowledge of both frontend and backend technologies, I’ve worked on various projects that reflect my ability to design, develop, and deploy complete web solutions. I'm also highly motivated to keep learning and growing in the tech field, which is why I’ve recently started exploring Artificial Intelligence to expand my skill set and future career opportunities.</p>
+                        </span>
+
+                        <span className='flex flex-col gap-3'>
+                            <h3 className='text-3xl font-semibold'>🎓 Education</h3>
+                            <p>
+                                - I'm currently pursuing my Bachelor's degree in Computer Science and actively expanding my skills in New Technologies.
+                            </p>
+                        </span>
+
+                        <span className='flex flex-col gap-3'>
+                            <h3 className='text-3xl font-semibold'>🚀 My next mission?</h3>
+                            <p>
+                                - Artificial Intelligence. I’m eager to expand my expertise into AI, exploring machine learning, deep learning, and intelligent systems to solve real-world challenges. Whether it’s coding, learning, or collaborating, I’m always ready for the next big problem to tackle.
+                            </p>
+                        </span>
+
 
                         
                     </div>
-                    <div className='w-1/2 !p-3 perspective-[1000px] relative flex place-content-center'>
-                        <Image src={pic} placeholder='blur' className='w-[80%] border-3 border-white rotate-y-[-10deg] transition-all'  alt='' />
+
+                    <div className='max-w-1/2 w-[45%] !p-3 perspective-[1000px] relative flex place-content-center'>
+                        <Image src={pic} placeholder='blur' className='w-[80%] border-3 border-white rotate-y-[10deg] transition-all'  alt='' />
                         <div className='
                         w-[80%] h-full border-2 border-white/70 absolute top-5
-                        rotate-y-[-10deg] z-[-1]
+                        rotate-y-[10deg] z-[-1]
                         '></div>
                         <div className='
                         w-[80%] h-full border-2 border-white/40 absolute top-8 right-6
-                        rotate-y-[-10deg] z-[-1]
+                        rotate-y-[10deg] z-[-1]
                         '></div>
                     </div>
                 </div>
@@ -28,28 +50,3 @@ export default function page() {
         </div>
     )
 }
-
-
-/*
-<span>
-    👋 Hi, I'm Syed Naveed Abbas from Skardu, Pakistan. 
-    <br />
-    I'm a passionate Full Stack Engineer with hands-on experience in <span className='text-main-skin-clr'> Next.js, MERN stack, Tailwind CSS, Bootstrap, and MySQL.</span>
-    
-
-    <p>
-        💻 I’ve developed multiple projects across the stack—from frontend interfaces to backend systems—sharpening my skills in modern technologies. Currently pursuing my Bachelor’s in Computer Science, I’m deeply curious about the intersection of software development and cutting-edge innovation.
-    </p>
-
-    I'm currently pursuing my Bachelor's degree in Computer Science and actively expanding my skills in New Technologies.
-    <br />
-    I love building modern, scalable web applications and continuously learning new technologies.
-</span>
-
-
-<p>
-    🚀 My next mission?
-    <br />    
-    Artificial Intelligence. I’m eager to expand my expertise into AI, exploring machine learning, deep learning, and intelligent systems to solve real-world challenges. Whether it’s coding, learning, or collaborating, I’m always ready for the next big problem to tackle.
-</p>
-*/
