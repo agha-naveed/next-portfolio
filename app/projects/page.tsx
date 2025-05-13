308101433f69277be5315f4b1e7e6c779edc52ec
agha-naveed <naveedabs31@gmail.com>
2025-05-13 14:38:32 +0500

    added 7up

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,7 @@ import Tilt from 'react-parallax-tilt';
 import Link from 'next/link'
 import store from 'public/img/projects/store.webp'
 import lms from 'public/img/projects/lib_lms.webp'
+import sevenup from 'public/img/projects/sevenup.webp'
 import chatbot from 'public/img/projects/chatbot.webp'
 
 export default function page() {
@@ -29,7 +30,6 @@ export default function page() {
                     </Tilt>
                 </div>
 
-
                 <div data-aos="fade-up" className='justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
                     <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] w-[80%] h-full relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
@@ -48,7 +48,6 @@ export default function page() {
 
                     </Tilt>
                 </div>
-
 
                 <div data-aos="fade-up" className='h-auto justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
@@ -69,13 +68,12 @@ export default function page() {
                     </Tilt>
                 </div>
 
-
                 <div data-aos="fade-up" className='h-auto justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
                     <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] h-full w-[80%] relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
                         
                         <div className='w-full overflow-hidden rounded-sm'>
-                            <Image src={chatbot} loading="lazy" alt='' className='w-full rounded-sm hover:scale-105 transition-all' placeholder='blur' />
+                            <Image src={sevenup} loading="lazy" alt='' className='w-full rounded-sm hover:scale-105 transition-all' placeholder='blur' />
                         </div>
 
                         <div className='flex flex-col !pt-8 !pb-15 gap-2'>
