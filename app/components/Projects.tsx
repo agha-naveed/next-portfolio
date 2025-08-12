b4d3e25c8b592e091ecfafcc59b63e55c32fa96d
Syed Naveed Abbas <naveedabs31@gmail.com>
2025-08-12 16:51:46 +0500

    added hover and update texts

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -32,7 +32,7 @@ export default function Projects() {
                     <div data-aos="fade-up" data-aos-delay="200" className='justify-items-center cursor-pointer'
                     data-aos-duration="1000" data-aos-easing="ease-out">
                         <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02}
-                        className='md:w-[350px] w-[80%] relative bg-secondary-light-clr !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[100]'>
+                        className='md:w-[350px] w-[80%] relative bg-secondary-light-clr hover:bg-[#16202b] !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[100]'>
 
                             <div className='w-full overflow-hidden rounded-sm'>
                                 <Image src={store} loading="lazy" alt='' className='w-full rounded-sm' placeholder='blur' />
@@ -40,7 +40,7 @@ export default function Projects() {
 
                             <div className='flex flex-col !py-8 gap-1'>
                                 <span className='text-[20px] font-semibold'>Lenmi Store</span>
-                                <span>Online Shopping Platform</span>
+                                <span>Shop anytime, anywhere—everything you need is just a click away.</span>
                             </div>
                             <Link href={"/projects/lenmi-store"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit !mb-5 absolute bottom-[16px] hover-text' title='View more about this Project'>
                                 <span className='relative z-[200]'>View More</span>
@@ -51,7 +51,7 @@ export default function Projects() {
 
                     <div data-aos="fade-up" data-aos-delay="350" className='justify-items-center cursor-pointer'
                     data-aos-duration="1000" data-aos-easing="ease-out">
-                        <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] w-[80%] relative bg-secondary-light-clr !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[100]'>
+                        <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] w-[80%] relative bg-secondary-light-clr hover:bg-[#16202b] !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[100]'>
 
                             <div className='w-full overflow-hidden rounded-sm'>
                                 <Image src={lms} loading="lazy" alt='' className='w-full rounded-sm' placeholder='blur' />
@@ -59,7 +59,7 @@ export default function Projects() {
 
                             <div className='flex flex-col !py-8 gap-1'>
                                 <span className='text-[20px] font-semibold'>Library Management System</span>
-                                <span>Desktop Application</span>
+                                <span>Simplify book tracking, borrowing, and inventory control.</span>
                             </div>
                             <Link href={"/projects/library-management-system"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit !mb-5 absolute bottom-[16px] hover-text' title='View more about this Project'>
                                 <span className='relative z-[200]'>View More</span>
@@ -70,7 +70,7 @@ export default function Projects() {
                     
                     <div data-aos="fade-up" data-aos-delay="500" className='justify-items-center cursor-pointer relative'
                     data-aos-duration="1000" data-aos-easing="ease-out">
-                        <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] w-[80%] relative bg-secondary-light-clr !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[1000]'>
+                        <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] w-[80%] relative bg-secondary-light-clr hover:bg-[#16202b] !pt-[26px] !pb-[90px] !px-5 rounded-lg z-[1000]'>
                             
                             <div className='w-full overflow-hidden rounded-sm'>
                                 <Image src={chatbot} loading="lazy" alt='' className='w-full rounded-sm' placeholder='blur' />
@@ -78,7 +78,7 @@ export default function Projects() {
 
                             <div className='flex flex-col !py-8 gap-1'>
                                 <span className='text-[20px] font-semibold'>AI Chatbot</span>
-                                <span>Agha Chatbot</span>
+                                <span>Your 24/7 digital assistant, ready to chat and help anytime.</span>
                             </div>
                             <Link href={"/projects/agha-chatbot"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit !mb-5 absolute bottom-[16px] hover-text' title='View more about this Project'>
                                 <span className='relative z-[200]'>View More</span>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
             <div className='flex flex-wrap justify-center relative gap-14 !py-[120px]'>
                 <div data-aos="fade-up" className='justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
-                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] w-[80%] h-full relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
+                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] w-[80%] h-full relative  bg-secondary-light-clr hover:bg-[#16202b] !py-[26px] !px-5 rounded-lg z-[1000]'>
                         
                         <div className='w-full overflow-hidden rounded-sm'>
                             <Image src={store} loading="lazy" alt='' className='w-full rounded-sm  transition-all' placeholder='blur' />
@@ -23,7 +23,7 @@ export default function page() {
 
                         <div className='flex flex-col !pt-8 !pb-15 gap-2'>
                             <span className='text-[22px] font-semibold'>Lenmi Store</span>
-                            <span className='text-[16px] leading-7'>Online Shopping Platform | Get Whatever you want</span>
+                            <span className='text-[15px] leading-7'>Your favorite brands, delivered to your door.</span>
                         </div>
                         <Link href={"/projects/lenmi-store"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit absolute bottom-6' title='View more about this Project'>
                             <span className='relative z-[200]'>View Project</span>
@@ -34,7 +34,7 @@ export default function page() {
 
                 <div data-aos="fade-up" className='justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
-                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] w-[80%] h-full relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
+                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] w-[80%] h-full relative  bg-secondary-light-clr hover:bg-[#16202b] !py-[26px] !px-5 rounded-lg z-[1000]'>
                         
                         <div className='w-full overflow-hidden rounded-sm'>
                             <Image src={aprDisDetect} loading="lazy" alt='' className='w-full rounded-sm  transition-all' placeholder='blur' />
@@ -42,7 +42,7 @@ export default function page() {
 
                         <div className='flex flex-col !pt-8 !pb-15 gap-2'>
                             <span className='text-[22px] font-semibold'>Apricot Disease Detector</span>
-                            <span className='text-[16px] leading-7'>Snap, upload, and know — AI-powered apricot health check.</span>
+                            <span className='text-[15px] leading-7'>Snap, upload, and know — AI-powered apricot health check.</span>
                         </div>
                         <Link href={"/projects/apricot-disease-detector"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit absolute bottom-6' title='View more about this Project'>
                             <span className='relative z-[200]'>View Project</span>
@@ -53,7 +53,7 @@ export default function page() {
 
                 <div data-aos="fade-up" className='justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
-                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] w-[80%] h-full relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
+                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] w-[80%] h-full relative  bg-secondary-light-clr hover:bg-[#16202b] !py-[26px] !px-5 rounded-lg z-[1000]'>
                         
                         <div className='w-full overflow-hidden rounded-sm'>
                             <Image src={lms} loading="lazy" alt='' className='w-full rounded-sm  transition-all' placeholder='blur' />
@@ -61,7 +61,7 @@ export default function page() {
 
                         <div className='flex flex-col !pt-8 !pb-15 gap-2'>
                             <span className='text-[22px] font-semibold'>Library Management System</span>
-                            <span className='text-[16px] leading-7'>Desktop Application for browsing, searching borrower etc.</span>
+                            <span className='text-[15px] leading-7'>Simplify book tracking, borrowing, and inventory control.</span>
                         </div>
                         <Link href={"/projects/library-management-system"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit absolute bottom-6' title='View more about this Project'>
                             <span className='relative z-[200]'>View Project</span>
@@ -72,7 +72,7 @@ export default function page() {
 
                 <div data-aos="fade-up" className='h-auto justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
-                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] h-full w-[80%] relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
+                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] h-full w-[80%] relative  bg-secondary-light-clr hover:bg-[#16202b] !py-[26px] !px-5 rounded-lg z-[1000]'>
                         
                         <div className='w-full overflow-hidden rounded-sm'>
                             <Image src={chatbot} loading="lazy" alt='' className='w-full rounded-sm  transition-all' placeholder='blur' />
@@ -80,7 +80,7 @@ export default function page() {
 
                         <div className='flex flex-col !pt-8 !pb-15 gap-2'>
                             <span className='text-[22px] font-semibold'>AI Chatbot</span>
-                            <span className='text-[16px] leading-7'>AI Chatbot for generating responses from inputs.</span>
+                            <span className='text-[15px] leading-7'>Agha AI Chatbot: Quick chats, smart answers.</span>
                         </div>
                         <Link href={"/projects/agha-chatbot"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit absolute bottom-6' title='View more about this Project'>
                             <span className='relative z-[200]'>View Project</span>
@@ -91,7 +91,7 @@ export default function page() {
 
                 <div data-aos="fade-up" className='h-auto justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
-                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] h-full w-[80%] relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
+                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] h-full w-[80%] relative  bg-secondary-light-clr hover:bg-[#16202b] !py-[26px] !px-5 rounded-lg z-[1000]'>
                         
                         <div className='w-full overflow-hidden rounded-sm'>
                             <Image src={sevenup} loading="lazy" alt='' className='w-full rounded-sm  transition-all' placeholder='blur' />
@@ -99,7 +99,7 @@ export default function page() {
 
                         <div className='flex flex-col !pt-8 !pb-15 gap-2'>
                             <span className='text-[22px] font-semibold'>7up Website Redesign</span>
-                            <span className='text-[16px] leading-7'>Refreshing experience that reflects the brand’s lively and youthful personality.</span>
+                            <span className='text-[15px] leading-7'>Refreshing experience that reflects the brand’s lively and youthful personality.</span>
                         </div>
                         <Link href={"/projects/7up-redesign"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit absolute bottom-6' title='View more about this Project'>
                             <span className='relative z-[200]'>View Project</span>
@@ -110,7 +110,7 @@ export default function page() {
 
                 <div data-aos="fade-up" className='h-auto justify-items-center cursor-pointer hover-text'
                 data-aos-duration="1000" data-aos-easing="ease-out">
-                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} className='md:w-[350px] h-full w-[80%] relative bg-secondary-light-clr !py-[26px] !px-5 rounded-lg z-[1000]'>
+                    <Tilt tiltMaxAngleX={5} tiltMaxAngleY={5} scale={1.02} className='md:w-[350px] h-full w-[80%] relative  bg-secondary-light-clr hover:bg-[#16202b] !py-[26px] !px-5 rounded-lg z-[1000]'>
                         
                         <div className='w-full overflow-hidden rounded-sm'>
                             <Image src={pharmacy} loading="lazy" alt='' className='w-full rounded-sm  transition-all' placeholder='blur' />
@@ -118,7 +118,7 @@ export default function page() {
 
                         <div className='flex flex-col !pt-8 !pb-15 gap-2'>
                             <span className='text-[22px] font-semibold'>Pharmacy Web App</span>
-                            <span className='text-[16px] leading-7'>Web Application for Pharmacy/Medical Store</span>
+                            <span className='text-[15px] leading-7'>Simplify daily tasks—track stock, process orders, and manage patients</span>
                         </div>
                         <Link href={"/projects/pharmacy-webapp"} className='bg-gradient !px-5 !py-[10px] rounded-xl bg-main-dark-clr font-medium text-[18px] border-[1px] border-[#465b7c] cursor-pointer transition-all w-fit absolute bottom-6' title='View more about this Project'>
                             <span className='relative z-[200]'>View Project</span>
